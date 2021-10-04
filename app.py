2af44a6aad6ad574b46c1d45f17cde0161e2c8ba
Igna-M <ign.manzo@gmail.com>
2021-10-04 17:54:58 -0300

    10 tweets ok

--- app.py
+++ app.py
@@ -54,19 +54,85 @@ def predict():
         tweet = request.form['tweet']
 
 
-
     cleaned_tweet = limpiar_tweets(tweet)
     pred = model.predict(vectorizer.transform([cleaned_tweet]))[0]
     
     if pred == 0:
         bloque = 'Juntos por el Cambio'
-        imagen = '../static/images/juntos-por-el-cambio-blanco-sm.png'
+        imagen = '../static/images/2560px-Juntos-Por-El-Cambio-Logo.png'
 
     else:
         bloque = 'Frente de Todos'
-        imagen = '../static/images/frente_de_todostodos_blanco.png'
+        imagen = '../static/images/2560px-Frente_de_Todos_logo.png'
 
-    return render_template('partido-politico-probable.html', bloque=bloque, imagen=imagen)
+    return render_template('partido-politico-probable.html', bloque=bloque, imagen=imagen, tweet=tweet)
+
+
+
+
+@app.route('/promedio-10-tweets', methods=['POST', 'GET'])
+def mean_10_tweets():
+
+
+
+    return render_template('promedio-10-tweets.html')
+
+
+
+@app.route('/predecir-promedio-10-tweets', methods=['POST', 'GET'])
+def mean_10_tweets_predict():
+
+    def limpiar_tweets(tweet):
+        t_lower_no_accents=unidecode.unidecode(tweet.lower()); # sacamos acentos y llevamos a minuscula
+        t_lower_no_accents_no_punkt=re.sub(r'([^\s\w]|_)+','',t_lower_no_accents); # quitamos signos de puntuacion usando una regex que reemplaza todo lo q no sean espacios o palabras por un string vacio
+        t_no_new_line = t_lower_no_accents_no_punkt.replace('\n', ' ')
+        t_remove_http = re.sub(r'http\S+', '', t_no_new_line).strip()
+        x = re.sub("(.)\\1{2,}", "\\1", t_remove_http)
+        return x
+
+    global vectorizer
+    global model
+
+    if request.method == 'POST':
+
+        print('')
+        print('')
+
+        todos_los_tweets = []
+        for i in range(1, 11):
+            tweet = request.form['tweet' + str(i)]
+            if len(tweet) > 0:
+                todos_los_tweets.append(tweet)
+
+        print('')
+        print(todos_los_tweets)
+        print('')
+
+        valoracion_de_tweets = []
+        for j in range(len(todos_los_tweets)):
+            print(todos_los_tweets[j])
+            cleaned_tweet = limpiar_tweets(todos_los_tweets[j])
+            pred = model.predict(vectorizer.transform([cleaned_tweet]))[0]
+            valoracion_de_tweets.append(pred)
+
+        promedio = sum(valoracion_de_tweets) / len(valoracion_de_tweets)
+        porcentaje = promedio * 100
+
+        porcentaje_jc = 50 + (50 - porcentaje)
+        porcentaje_ft = 50 + (porcentaje - 50)
+        
+        len_tweets = len(todos_los_tweets)
+
+        if promedio < 0.5:
+            bloque = 'Juntos por el Cambio'
+            imagen = '../static/images/2560px-Juntos-Por-El-Cambio-Logo.png'
+            grado = porcentaje_jc
+        else:
+            bloque = 'Frente de Todos'
+            imagen = '../static/images/2560px-Frente_de_Todos_logo.png'
+            grado = porcentaje_ft
+
+    return render_template('promedio-10-tweets-resultado.html', bloque=bloque, imagen=imagen, promedio=promedio, tweets=todos_los_tweets, len_tweets=len_tweets, grado=grado)
 
 
 
